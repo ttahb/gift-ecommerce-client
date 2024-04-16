6be47185c5d38f55fa0cdb1ed981f0c36661925d
Pencho Ivanov <penchoivanov@Penchos-MacBook-Air.local>
2024-04-16 14:12:15 +0200

    creting product async

--- src/pages/ProductCreatePage.jsx
+++ src/pages/ProductCreatePage.jsx
@@ -9,15 +9,15 @@ function ProductCreatePage() {
     const [ productName, setProductName ] = useState('');
     const [ price, setPrice ] = useState('');
     const [ description, setDescription ] = useState('');
-    const [ image, setImage ] = useState('');
     const [ tag, setTag ] = useState('');
     const [ errorMsg, setErrorMsg ] = useState(undefined);
     const [ priceLetter, setPriceLetter ] = useState(undefined);
     const { user } = useContext(AuthContext);
+    const [ isloading , setIsLoading ] = useState(false);
     const navigate = useNavigate();
 
+    // useState set...
     const handleProductName = (e) => setProductName(e.target.value);
-
     const rexegLetters = /[A-Za-z]/;
     const rexegNumbers = /[0-9]/;
     const handlePrice = (e) => {
@@ -32,29 +32,12 @@ function ProductCreatePage() {
             setPrice(e.target.value);
         }
     }
-        
     const handleDescription = (e) => setDescription(e.target.value);
     const handleTag = (e) => setTag(e.target.value);
 
-    const handleFileUpload = (e) => {
+    // handleEvent
 
-        const uploadData = new FormData();
-
-        uploadData.append("img", e.target.files[0]);
-        console.log('this is the nploadData NEW FORM ==>', uploadData)
-
-        fileUploadService
-            .uploadImage(uploadData)
-            .then(res => {
-                console.log('the is the response from .then ==>', res.image)
-                setImage(res.image);
-            })
-            .catch(err => console.log('error back from the server',err))
-    }
-
-
-
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         if(rexegLetters.test(price)){
@@ -62,33 +45,53 @@ function ProductCreatePage() {
 
         } else if(rexegNumbers.test(price)) {
             setErrorMsg(undefined);
+        
+            try{
+                setIsLoading(true)
+                const uploadData = new FormData();
+                uploadData.append("img", e.target[3].files[0]);
+                    // console.log('this is the nploadData NEW FORM ==>', uploadData , e.target[3].files[0])
 
-            const reqBody = {
-                userId: user.userId,
-                productName,
-                price,
-                image,
-                description,
-                tags: tag
+                const uploadInfo = await fileUploadService.uploadImage(uploadData)
+                    // console.log("uploadedImage",uploadInfo)
+                const uploadedImage = uploadInfo.image
+                    // console.log("uploadedImage",uploadedImage)
+
+                const reqBody = {
+                    userId: user.userId,
+                    productName,
+                    price,
+                    image: uploadedImage,
+                    description,
+                    tags: tag
+                }
+
+                const creationProd = await productsService.createProduct(reqBody)
+                    // console.log("created product",creationProd)
+                const productId = creationProd.data._id;
+                setIsLoading(false)
+                navigate(`/product/${productId}`)
+
+            }catch(err){
+                setErrorMsg(err.response.data.message);
             }
 
-            // console.log('JUST BEFORE THE PRODUCTAS SERVICE ==>',reqBody)
-            productsService
-                .createProduct(reqBody)
-                .then((res) => {
-                    // console.log('response of the BACK END ===>', res.data)
-                    const produstId = res.data._id;
-                    navigate(`/product/${produstId}`)
-                })
-                .catch((err) => {
-                    setErrorMsg(err.response.data.message);
-                })
-            }
+        }
+    }
+
+    if(isloading){
+        return (
+            <div>
+                <span className="loader"></span>
+                <p>Loading...</p>
+            </div>
+        )
     }
 
     return(
         <div className="auth-form">
             { errorMsg && <p>error: {errorMsg}</p>}
+
             <form onSubmit={handleSubmit}>
                 <label> Name:
                 <br />
@@ -108,23 +111,25 @@ function ProductCreatePage() {
                 <br />
                 <label>Upload URL image: 
                     <br />
-                    <input type="file" onChange={handleFileUpload} />
-                </label>
-                <br />
-                <label>Tag:
-                <br />
-                    <select name="tags" onChange={handleTag}>
-                        <option value=""></option>
-                        <option value="wine">Wine</option>
-                        <option value="jams">Jams</option>
-                        <option value="chocolates">Chocolates</option>
-                        <option value="cookies">Cookies</option>
-                        <option value="cakes">Cakes</option>
-                    </select>
-                </label>
-                <br />
-                <button>Create</button>
-            </form>
+                    <input type="file"/>
+                    {/* onChange={handleFileUpload} */}
+                    </label>
+                    <br />
+                    <label>Tag:
+                    <br />
+                        <select name="tags" onChange={handleTag}>
+                            <option value=""></option>
+                            <option value="wine">Wine</option>
+                            <option value="jams">Jams</option>
+                            <option value="chocolates">Chocolates</option>
+                            <option value="cookies">Cookies</option>
+                            <option value="cakes">Cakes</option>
+                        </select>
+                    </label>
+                    <br />
+                    <button>Create</button>
+                </form>
+            
         </div>
     )
 }
